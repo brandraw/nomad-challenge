701db303f316ecc87dd18db460bbd1c4c76d013c
창욱 <changwook@chang-ug-ui-MacBookAir.local>
2024-07-11 15:46:14 +0900

    add userdata and date in tweet list

--- app/_components/tweet-list.tsx
+++ app/_components/tweet-list.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { initialTweets } from "../page";
+import { UserCircleIcon } from "@heroicons/react/16/solid";
 
 interface tweetProps {
   initialTweets: initialTweets;
@@ -14,9 +15,24 @@ export function TweetList({ initialTweets }: tweetProps) {
         <Link
           href={`/tweets/${a.id}`}
           key={a.id}
-          className="border rounded-lg shadow-sm p-5 flex justify-between transition hover:-translate-y-1 hover:shadow-md"
+          className="border rounded-lg shadow-sm p-5 flex flex-col justify-between transition hover:-translate-y-1 hover:shadow-md"
         >
           <span>{a.tweet}</span>
+          <div className="flex items-center gap-2 justify-between border-t pt-4 mt-4">
+            <div>
+              <span className="text-xs text-neutral-400">
+                {a.created_at.toLocaleDateString()}
+              </span>
+            </div>
+            <div className="flex items-center justify-end gap-2">
+              <div className="relative size-6 aspect-square overflow-hidden rounded-full">
+                <UserCircleIcon className="size-full text-neutral-400" />
+              </div>
+              <span className="text-sm text-neutral-400 font-medium">
+                {a.user.username}
+              </span>
+            </div>
+          </div>
         </Link>
       ))}
     </div>
